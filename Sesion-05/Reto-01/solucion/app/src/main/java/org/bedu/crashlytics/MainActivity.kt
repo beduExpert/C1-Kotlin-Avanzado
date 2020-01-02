b34edd27ffae1dc821523780b345fd35d85fe161
Daniel Coutiño <ing.dantonio@gmail.com>
2020-01-02 03:36:59 -0600

    Sesion 5 Reto 1 terminado

---
+++ Sesion-05/Reto-01/solucion/app/src/main/java/org/bedu/crashlytics/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.bedu.crashlytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnError.setOnClickListener{
+            try {
+                throw NullPointerException()
+            } catch (ex: NullPointerException) {
+                Crashlytics.log(Log.ERROR, "CrashError", "NullPointer Provocado para pruebas!")
+            }
+        }
+    }
+}
